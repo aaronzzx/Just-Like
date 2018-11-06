35e13d28f1f01a8f9981793141de7d71dc38cf94
Aaron <aaronzzxup@gmail.com>
2018-11-07 02:55:04 +0800

    UI

--- app/src/main/java/com/aaron/justlike/activity/DisplayImageActivity.java
+++ app/src/main/java/com/aaron/justlike/activity/DisplayImageActivity.java
@@ -76,6 +76,9 @@ public class DisplayImageActivity extends AppCompatActivity {
         super.onWindowFocusChanged(hasFocus);
         if (hasFocus) {
             Window window = getWindow();
+            window.clearFlags(WindowManager.LayoutParams.FLAG_TRANSLUCENT_STATUS);
+//            window.addFlags(WindowManager.LayoutParams.FLAG_DRAWS_SYSTEM_BAR_BACKGROUNDS);
+//            window.setStatusBarColor(Color.TRANSPARENT);
             window.getDecorView().setSystemUiVisibility(View.SYSTEM_UI_FLAG_LAYOUT_STABLE
                     | View.SYSTEM_UI_FLAG_LAYOUT_HIDE_NAVIGATION
                     | View.SYSTEM_UI_FLAG_LAYOUT_FULLSCREEN);
